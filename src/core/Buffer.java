0aa0c99687fd66f1f38b061d25152f62c80b0059
SmeeK153 <eric.freeman@ago.org>
2017-05-03 18:50:31 -0700

    Created Buffer wrapper on BufferedReader and BufferedWriter.

---
+++ src/core/Buffer.java
@@ -0,0 +1,41 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URLConnection;
+
+public class Buffer {
+	
+	private Buffer(){}
+	
+	public static void print(InputStream stream){
+		try {
+			System.out.println(Buffer.read(stream));
+		} catch (IOException e) {
+			System.err.println("Could not print requested stream, an exception occurred.");
+			e.printStackTrace();
+		}
+	}
+	
+	public static String read(InputStream stream) throws IOException{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		String line = null;
+		String content = "";
+		while((line = reader.readLine()) != null){
+			content += line;
+		}
+		return content;
+	}
+	
+	public static void write(OutputStream stream, String content) throws IOException{
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));
+		writer.write(content);
+		writer.close();
+	}
+	
+}
